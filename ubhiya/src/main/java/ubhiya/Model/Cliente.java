326ddca264ba730bc362e43c3d4040e7722e0c2b
felipe <felipe@ifMota>
2018-10-12 00:48:31 -0300

    Implementação das páginas Index, Adm, Cliente, Comércio e Fabricante.

---
+++ ubhiya/src/main/java/ubhiya/Model/Cliente.java
@@ -0,0 +1,147 @@
+package ubhiya.Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import ubhiya.ConexaoMySQL;
+
+public class Cliente extends Usuario{
+	
+	private Long idCliente;
+	private String nome;
+	private String endereco;
+	public String status;
+	
+	public Cliente() {
+	}
+	
+	public Cliente(Usuario u, Long idCliente, String nome, String endereco) {
+		this.setId(u.getId());
+		this.setLogin(u.getLogin());
+		this.setSenha(u.getSenha());
+		this.idCliente = idCliente;
+		this.nome = nome;
+		this.endereco = endereco;
+	}
+
+	public void cadastrarCliente() {
+		Long idC = this.cadastrar(this.getLogin(), this.getSenha(), 2);
+		
+		new ConexaoMySQL();
+		Connection con = ConexaoMySQL.conectar();
+		
+		String sql = "insert into Cliente (Usuario_id_usuario, nome, endereco) values (?,?,?)";
+
+		try{
+			PreparedStatement stmt = con.prepareStatement(sql);	
+			stmt.setLong(1, idC);
+			stmt.setString(2, this.nome);
+			stmt.setString(3, this.endereco);
+			stmt.execute();
+			stmt.close();
+
+			this.status = "Usuário "+this.getLogin()+" inserido com sucesso";
+			con.close();
+		}catch(Exception e) {
+			System.out.println("Inclusão não realizada: "+e);
+			status = "Problema na inclusão: " +idC +e;
+		}
+	}
+	
+	public void deletarCliente(Long id) {
+		new ConexaoMySQL();
+		Connection con = ConexaoMySQL.conectar();
+		String sql = "delete from Cliente where Usuario_id_usuario = ?";
+
+		try{
+			PreparedStatement stmt = con.prepareStatement(sql);		
+			stmt.setLong(1, id);
+			stmt.execute();
+			stmt.close();
+
+			status = "Usuário excluído com sucesso";
+			con.close();
+		}catch(Exception e) {
+			System.out.println("Exclusão não realizada: "+e);
+			status = "Problema na exclusão: "+e;
+		}
+	}
+	
+	public void alterarCliente() {
+		new ConexaoMySQL();
+		Connection con = ConexaoMySQL.conectar();
+		
+		this.alterar();
+		String sql = "update Cliente set nome = ?, endereco = ? where Usuario_id_usuario = ?";
+
+		try{
+			PreparedStatement stmt = con.prepareStatement(sql);		
+			stmt.setString(1, this.nome);
+			stmt.setString(2, this.endereco);
+			stmt.setLong(3, this.getId());
+			stmt.execute();
+			stmt.close();
+
+			this.status = "Usuário "+ this.getLogin() + " alterado com sucesso";
+			con.close();
+		}catch(Exception e) {
+			System.out.println("Exclusão não realizada: "+e);
+			this.status = "Problema na alteração: "+e;
+		}
+	}
+	
+	public List<Cliente> buscarClientes() {
+		Connection con = ConexaoMySQL.conectar();
+		String sql = "select a.id_usuario, a.login, a.senha, b.id_cliente, b.nome, b.endereco from Usuario as a, Cliente as b where b.Usuario_id_usuario = a.id_usuario";
+
+		List<Cliente> clientes = new ArrayList<Cliente>(); 
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				Usuario u = new Usuario(rs.getLong("id_usuario"), rs.getString("login"), rs.getString("senha"), 2);
+				Cliente c = new Cliente(u, rs.getLong("id_cliente"), rs.getString("nome"), rs.getString("endereco"));
+				clientes.add(c);
+			}
+			rs.close();
+			stmt.close();
+		} catch (SQLException e) {
+			this.status = "Problema na Pesquisa: "+e;
+		}
+		return clientes;		
+	}
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	
+}
---
+++ ubhiya/src/main/java/ubhiya/Model/Fabricante.java
@@ -0,0 +1,170 @@
+package ubhiya.Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import ubhiya.ConexaoMySQL;
+
+public class Fabricante  extends Usuario{
+
+	private Long idComercio;
+	private String nomeFantasia;
+	private String razaoSocial;
+	private String cnpj;
+	private String ie;
+	public String status;
+
+	public Fabricante() {
+	}
+
+	public Fabricante(Usuario u, Long idComercio, String razaoSocial, String cnpj, String ie, String nomeFantasia) {
+		this.setId(u.getId());
+		this.setLogin(u.getLogin());
+		this.setSenha(u.getSenha());
+		this.idComercio = idComercio;
+		this.nomeFantasia = nomeFantasia;
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+		this.ie = ie;
+	}
+
+	public void cadastrarComercio() {
+		Long idC = this.cadastrar(this.getLogin(), this.getSenha(), 3);
+
+		new ConexaoMySQL();
+		Connection con = ConexaoMySQL.conectar();
+
+		String sql = "insert into Fabricante (Usuario_id_usuario, razao_social, CNPJ, IE, nome_fantasia) values (?,?,?,?,?)";
+
+		try{
+			PreparedStatement stmt = con.prepareStatement(sql);	
+			stmt.setLong(1, idC);
+			stmt.setString(2, this.razaoSocial);
+			stmt.setString(3, this.cnpj);
+			stmt.setString(4, this.ie);
+			stmt.setString(5, this.nomeFantasia);
+			stmt.execute();
+			stmt.close();
+
+			this.status = "Usuário "+this.getLogin()+" inserido com sucesso";
+			con.close();
+		}catch(Exception e) {
+			System.out.println("Inclusão não realizada: "+e);
+			status = "Problema na inclusão: " +idC +e;
+		}
+	}
+
+	public void deletarComercio(Long id) {
+		new ConexaoMySQL();
+		Connection con = ConexaoMySQL.conectar();
+		String sql = "delete from Fabricante where Usuario_id_usuario = ?";
+
+		try{
+			PreparedStatement stmt = con.prepareStatement(sql);		
+			stmt.setLong(1, id);
+			stmt.execute();
+			stmt.close();
+
+			status = "Usuário excluído com sucesso";
+			con.close();
+		}catch(Exception e) {
+			System.out.println("Exclusão não realizada: "+e);
+			status = "Problema na exclusão: "+e;
+		}
+	}
+
+	public void alterarComercio() {
+		new ConexaoMySQL();
+		Connection con = ConexaoMySQL.conectar();
+
+		this.alterar();
+		String sql = "update Fabricante set razao_social = ?, CNPJ = ?, IE = ?, nome_fantasia = ? where Usuario_id_usuario = ?";
+
+		try{
+			PreparedStatement stmt = con.prepareStatement(sql);		
+			stmt.setString(1, this.razaoSocial);
+			stmt.setString(2, this.cnpj);
+			stmt.setString(3, this.ie);
+			stmt.setString(4, this.nomeFantasia);
+			stmt.setLong(5, this.getId());
+			stmt.execute();
+			stmt.close();
+
+			this.status = "Usuário "+ this.getLogin() + " alterado com sucesso";
+			con.close();
+		}catch(Exception e) {
+			System.out.println("Exclusão não realizada: "+e);
+			this.status = "Problema na alteração: "+e;
+		}
+	}
+
+	public List<Fabricante> buscarComercios() {
+		Connection con = ConexaoMySQL.conectar();
+		String sql = "select a.id_usuario, a.login, a.senha, b.id_fabricante, b.razao_social, b.CNPJ, b.IE, b.nome_fantasia from Usuario as a, Fabricante as b where b.Usuario_id_usuario = a.id_usuario";
+
+		List<Fabricante> comercios = new ArrayList<Fabricante>(); 
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				Usuario u = new Usuario(rs.getLong("id_usuario"), rs.getString("login"), rs.getString("senha"), 3);
+				Fabricante c = new Fabricante(u, rs.getLong("id_fabricante"), rs.getString("razao_social"), rs.getString("CNPJ"), rs.getString("IE"), rs.getString("nome_fantasia"));
+				comercios.add(c);
+			}
+			rs.close();
+			stmt.close();
+		} catch (SQLException e) {
+			this.status = "Problema na Pesquisa: "+e;
+		}
+		return comercios;		
+	}
+
+	public Long getIdComercio() {
+		return idComercio;
+	}
+
+	public void setIdComercio(Long idComercio) {
+		this.idComercio = idComercio;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getIe() {
+		return ie;
+	}
+
+	public void setIe(String ie) {
+		this.ie = ie;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+}
